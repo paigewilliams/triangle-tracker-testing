0ddba1660f9adfe15fe13c3ae639595883f945ba
Paige Williams and Ralph Perdomo <paige@>
2018-10-18 15:19:03 -0700

    complete business logic of triangle calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,46 @@
 $(function() {
+  $("#calculate").click(function(){
+    var sideA = parseInt($("#side-a").val());
+    var sideB = parseInt($("#side-b").val());
+    var sideC = parseInt($("#side-c").val());
+    var triangleStatus;
 
+    if (isTrianle(sideA,sideB,sideC)) {
+      if (scalene(sideA,sideB,sideC)){
+        triangleStatus ="Scalene";
+      }
+      if (isosceles(sideA,sideB,sideC)) {
+        triangleStatus = "Isosceles";
+      }
+      if (equilateral(sideA,sideB,sideC)) {
+        triangleStatus = "Equilateral";
+      }
+    } else {triangleStatus = "Not a triangle"}
+
+    console.log(triangleStatus)
+
+  })
 
 
 })
+
+var equilateral = function(a,b,c) {
+  if (a===b && b===c && a===c) {
+    return true
+  } return false
+}
+var isosceles = function(a,b,c) {
+  if (a===b || b===c || a===c) {
+    return true
+  } return false
+}
+var scalene = function(a,b,c) {
+  if (a!==b && b!==c && a!==c) {
+    return true
+  } return false
+}
+var isTrianle = function(a,b,c) {
+  if ( a+b>c && b+c>a && a+c>b){
+    return true
+  } return false
+}
